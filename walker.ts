7204c8b3037a02db229092d04f6b80db01a62bb0
Chapi <j.chateau@oodrive.com>
2019-09-12 22:34:46 +0200

    add random walker 8 directions

---
+++ walker.ts
@@ -0,0 +1,24 @@
+import p5 from 'p5';
+
+export default class Walker {
+	x: number = this._x;
+	y: number = this._y;
+
+	constructor(
+		private p: p5,
+		private _x: number,
+		private _y: number,
+	) {}
+
+	display(): void {
+		this.p.stroke(0);
+		this.p.ellipse(this.x, this.y, 10);
+	}
+
+	step(): void {
+		const stepX = Math.floor(Math.random() * 3) - 1;
+		const stepY = Math.floor(Math.random() * 3) - 1;
+		this.x = this.x + stepX;
+		this.y = this.y + stepY;
+	}
+}
