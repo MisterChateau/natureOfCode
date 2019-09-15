233d245e0bcf2bf220754f1812824a13b50b5b4d
Chapi <j.chateau@oodrive.com>
2019-09-15 11:09:20 +0200

    randow walker with noise and random distribution

--- index.ts
+++ index.ts
@@ -1,33 +1,4 @@
 import p5 from 'p5';
-import Walker from './walker';
+import walkerSketch from './walker/index';
 
-const WIDTH =  1000;
-const HEIGHT = 1000;
-
-let walkerMax = 1;
-document.addEventListener ('DOMContentLoaded', () => {
-	const control = document.querySelector('input');
-	control.addEventListener('change', () => (walkerMax = Number(control.value)));
-});
-
-function sketch(p: p5) {
-	let history: Walker[] = [];
-
-	p.setup = () => {
-		p.createCanvas(WIDTH, HEIGHT, 'p2d');
-		p.background('#B66DE8');
-		p.smooth();
-	}
-
-	p.draw = () => {
-		const tail = history[history.length - 1];
-
-		const walker: Walker = history.length ? new Walker(p, tail.x, tail.y) : new Walker(p, WIDTH/2, HEIGHT/2);
-
-		if (history.length < walkerMax) history.push(walker);
-
-		history.forEach((walker) => (walker.step(), walker.display()));
-	}
-}
-
-new p5(sketch);
+new p5(walkerSketch);
---
+++ walker/index.ts
@@ -0,0 +1,32 @@
+import p5 from 'p5';
+import Walker from './walker';
+
+let walkerMax = 10;
+document.addEventListener ('DOMContentLoaded', () => {
+	const control = document.querySelector('input');
+	control.addEventListener('change', () => (walkerMax = Number(control.value)));
+});
+
+export default function sketch(p: p5) {
+	let history: Walker[] = [];
+
+	const WIDTH =  p.windowWidth;
+	const HEIGHT = p.windowHeight;
+
+	p.setup = () => {
+		p.createCanvas(WIDTH, HEIGHT, 'p2d');
+		p.background('#B66DE8');
+		p.smooth();
+	}
+
+	p.draw = () => {
+		const tail = history[history.length - 1];
+
+		const walker: Walker = history.length ? new Walker(p, tail.x, tail.y) : new Walker(p, WIDTH/2, HEIGHT/2);
+
+		if (history.length < walkerMax) history.push(walker);
+
+		history.forEach((walker) => (walker.step(), walker.display()));
+	}
+}
+
